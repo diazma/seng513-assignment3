1732061ad7dd8f49600429cc57576f25c5be3343
Maria Diaz <diazma@ucalgary.ca>
2017-02-28 22:17:07 -0700

    Adding nicks with color, changing nick and changing color. WIP

--- index.js
+++ index.js
@@ -10,24 +10,50 @@ http.listen( port, function () {
 
 app.use(express.static(__dirname + '/public'));
 var usersConnected = [];
+var coloursUsed = [];
 var usersAvailable = ["Ruby", "Toulouse", "Dexter", "Mr. Mittens", "Warren the office cat"];
-var usernames = {};
+var availableColours = ["#ff3399", "#6600ff", "#339933", "#ff9900", "#cc0000"];
 // listen to 'chat' messages
 io.on('connection', function(socket){
     socket.on('sendmessage', function(msg){
-	io.emit('updatechat', Date().toString().split(' ')[4], socket.username, msg);
+        // Handle message sent from client
+        let msgArray = msg.split(" ");
+        if (msg.indexOf("/nick ") >= 0 && msgArray.length >= 1) { // Change nick
+            let oldName = socket.username;
+            let arrayLength = msgArray.length;
+            socket.username = msgArray.slice(1, arrayLength).join(" ");
+            socket.emit('assignednickname', socket.username, socket.colour);
+            io.emit('updatechat', Date().toString().split(' ')[4], 'SERVER', 'user ' + oldName +
+                ' has changed nickname to ' + socket.username, "#000000");
+        }
+        else if (msg.indexOf("/nickcolor ") >= 0 &&
+            msgArray.length == 2  &&
+            /(^[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i.test(msgArray[1])){
+            // Change color
+            socket.colour = msgArray[1];
+            socket.emit('assignednickname', null, "#" + socket.colour);
+            socket.emit('updatechat', Date().toString().split(' ')[4], 'SERVER', 'You have changed successfully your nick color',
+                "#000000");
+        }
+        else {
+            io.emit('updatechat', Date().toString().split(' ')[4], socket.username, msg, socket.colour);
+        }
+
     });
 
     // Assign a random username
     socket.on('userjoined', function() {
 
-        let index = Math.floor(Math.random()*usersAvailable.length)
+        let index = Math.floor(Math.random()*usersAvailable.length);
         usersConnected.push(usersAvailable[index]);
         socket.username = usersAvailable[index];
+        let colorIndex = Math.floor(Math.random()*usersAvailable.length);
+        socket.colour = availableColours[colorIndex];
         usersAvailable.splice(index, 1);
-        io.emit('assign-nick', socket.username);
-        socket.emit('updatechat', Date().toString().split(' ')[4], 'SERVER', 'You are user: ' + socket.username );
-        io.emit('updatechat', Date().toString().split(' ')[4], 'SERVER', socket.username + ' has connected to chat');
+        availableColours.splice(colorIndex, 1);
+        socket.emit('assignednickname', socket.username, socket.colour);
+        socket.emit('updatechat', '', 'SERVER NOTIFICATION', 'You are user: ' + socket.username, "#000000");
+        io.emit('updatechat', Date().toString().split(' ')[4], 'SERVER', socket.username + ' has connected to chat', "#000000");
     });
 
 });
--- public/client.js
+++ public/client.js
@@ -1,20 +1,36 @@
 // shorthand for $(document).ready(...)
 
-var currentNick = null;
-
+var currentNickname = null;
+var currentColour = null;
 $(function() {
     var socket = io.connect('http://localhost:3000');
         $('form').submit(function(){
-        socket.emit('sendmessage', $('#m').val());
-        $('#m').val('');
+            // Sending messages to the server
+            socket.emit('sendmessage', $('#m').val());
+            $('#m').val('');
         return false;
     });
-    socket.on('updatechat', function(timestamp, username, msg){
-	    $('#messages').append($('<li>').text(timestamp + ' ' + username + ': ' + msg));
+    socket.on('updatechat', function(timestamp, username, msg, colour){
+        // When the user is the current user, add colour
+        if (username == currentNickname) {
+            $('#messages').append($('<li>').html('<b>' + timestamp + ' ' + '<span style="color: '+ currentColour + '">'
+                + username + ':</span> ' + msg + '</b>'));
+        }
+        else {
+            $('#messages').append($('<li>').html(timestamp + ' ' + '<span style="color: '+ colour + '">'
+                + username + ':</span> ' + msg));
+        }
+
     });
 
-    socket.on('nicknames', function(users) {
-       usersAvailable = users;
+    socket.on('assignednickname', function(nickname, hexColour) {
+        if (nickname) {
+            currentNickname = nickname;
+        }
+        if (hexColour) {
+            currentColour = hexColour;
+        }
+
     });
 
     // User has joined to the chat upon connect
