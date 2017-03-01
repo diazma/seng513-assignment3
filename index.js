635adcead75d04a0a4da84002103c75dee786be9
Maria Diaz <diazma@ucalgary.ca>
2017-02-28 20:33:10 -0700

    Adding 5 default usernames and assign them automatically when user joins

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ http.listen( port, function () {
 });
 
 app.use(express.static(__dirname + '/public'));
-
+var usersConnected = [];
+var usersAvailable = ["Ruby", "Toulouse", "Dexter", "Mr. Mittens", "Warren the office cat"];
 var usernames = {};
 // listen to 'chat' messages
 io.on('connection', function(socket){
@@ -17,9 +18,16 @@ io.on('connection', function(socket){
 	io.emit('updatechat', Date().toString().split(' ')[4], socket.username, msg);
     });
 
-    socket.on('userjoined', function(username) {
-        socket.username = username;
-        usernames[username] = username;
-        io.emit('updatechat', Date().toString().split(' ')[4], 'SERVER', username + ' has connected to chat');
-    })
+    // Assign a random username
+    socket.on('userjoined', function() {
+
+        let index = Math.floor(Math.random()*usersAvailable.length)
+        usersConnected.push(usersAvailable[index]);
+        socket.username = usersAvailable[index];
+        usersAvailable.splice(index, 1);
+        io.emit('assign-nick', socket.username);
+        socket.emit('updatechat', Date().toString().split(' ')[4], 'SERVER', 'You are user: ' + socket.username );
+        io.emit('updatechat', Date().toString().split(' ')[4], 'SERVER', socket.username + ' has connected to chat');
+    });
+
 });
--- public/client.js
+++ public/client.js
@@ -1,16 +1,25 @@
 // shorthand for $(document).ready(...)
+
+var currentNick = null;
+
 $(function() {
     var socket = io.connect('http://localhost:3000');
-    $('form').submit(function(){
-	socket.emit('sendmessage', $('#m').val());
-	$('#m').val('');
-	return false;
+        $('form').submit(function(){
+        socket.emit('sendmessage', $('#m').val());
+        $('#m').val('');
+        return false;
     });
     socket.on('updatechat', function(timestamp, username, msg){
 	    $('#messages').append($('<li>').text(timestamp + ' ' + username + ': ' + msg));
     });
 
-    socket.on('connect', function() {
-        socket.emit('userjoined', prompt("What's your name?"));
+    socket.on('nicknames', function(users) {
+       usersAvailable = users;
     });
+
+    // User has joined to the chat upon connect
+    socket.on('connect', function() {
+        socket.emit('userjoined');
+    });
+
 });
